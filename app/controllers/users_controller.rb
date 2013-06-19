8202c0c4096081c2f812714ab292971e89e5e527
Casey McCourt <cas.mcco@gmail.com>
2013-06-19 12:31:32 -0700

    chapter 9 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
+  before_filter :new_user,       only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
@@ -61,6 +62,10 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def new_user
+      redirect_to(root_path) if signed_in?
     end
 
 end
